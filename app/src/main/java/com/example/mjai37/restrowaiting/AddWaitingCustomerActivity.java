ee6e2bcb4266385686f7a42644d7ac86cf9cd81a
Motishjain <motishj@ymail.com>
2016-01-22 08:25:28 +0530

    config

--- app/src/main/java/com/example/mjai37/restrowaiting/AddWaitingCustomerActivity.java
+++ app/src/main/java/com/example/mjai37/restrowaiting/AddWaitingCustomerActivity.java
@@ -1,29 +1,34 @@
 package com.example.mjai37.restrowaiting;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.app.Activity;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
-public class AddWaitingCustomerActivity extends AppCompatActivity {
+public class AddWaitingCustomerActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_customer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_add_waiting_customer);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //TODO: what does this line does??
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        Button addButton = (Button) findViewById(R.id.add_button);
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+        return true;
     }
 
 }
